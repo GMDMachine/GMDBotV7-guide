657963005d1d903225770f7818a1ccc6734a06f1
GMDMachine <161696961+GMDMachine@users.noreply.github.com>
2024-03-06 12:56:26 +0900

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,15 +4,15 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 const commands = [];
-// Grab all the command folders from the commands directory you created earlier
+// ./commands/경로에 있는 모든 명령어 가져오기
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-	// Grab all the command files from the commands directory you created earlier
+	// ./commands/경로에 있는 모든 명령어 가져오기
 	const commandsPath = path.join(foldersPath, folder);
 	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+	// SlashCommandBuilder로 명령어 가져오기
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
@@ -24,15 +24,15 @@ for (const folder of commandFolders) {
 	}
 }
 
-// Construct and prepare an instance of the REST module
+// REST 모듈 설정하기
 const rest = new REST().setToken(token);
 
-// and deploy your commands!
+// 명령어 새로고침 시작!
 (async () => {
 	try {
 		console.log(`(${commands.length})개의 슬래시(/)명령어 새로고침을 시작합니다.`);
 
-		// The put method is used to fully refresh all commands in the guild with the current set
+		// 서버에 명령어 등록하기
 		const data = await rest.put(
 			Routes.applicationGuildCommands(clientId, guildId),
 			{ body: commands },
@@ -40,7 +40,7 @@ const rest = new REST().setToken(token);
 
 		console.log(`(${data.length})개의 슬래시(/)명령어 새로고침을 성공했어요!`);
 	} catch (error) {
-		// And of course, make sure you catch and log any errors!
+		// 에러 모두 기록하기
 		console.error(error);
 	}
 })();
